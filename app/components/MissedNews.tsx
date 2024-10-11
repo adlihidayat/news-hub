105b2b6f5e62adda1dd6073492a180758b9bb910
unknown <adlihidayat30@gmail.com>
2024-10-11 18:07:15 +0700

    fixed missed news section

--- app/components/MissedNews.tsx
+++ app/components/MissedNews.tsx
@@ -31,7 +31,7 @@ const NewsItem: React.FC<NewsItemProps> = ({ data, title, id }) => {
       onClick={handler}
       className="w-full md:w-auto flex-1 flex flex-col items-center border-2 border-black p-5 rounded-[40px] relative overflow-hidden group cursor-pointer"
     >
-      <div className="w-full h-full bg-slate-500 rounded-[40px] overflow-hidden absolute rotate-90 top-0 translate-y-80 md:group-hover:translate-y-0 md:group-hover:rotate-0 animation-smooth-500">
+      <div className="w-full h-full bg-slate-500 rounded-[40px] overflow-hidden absolute rotate-90 top-0 translate-y-96 md:group-hover:translate-y-0 md:group-hover:rotate-0 animation-smooth-500">
         <Image
           priority
           unoptimized
@@ -53,7 +53,7 @@ const NewsItem: React.FC<NewsItemProps> = ({ data, title, id }) => {
         <div className="w-full h-[2px] bg-black"></div>
         <div className="w-3 h-3 rounded-full bg-black"></div>
       </div>
-      <h2 className="leading-[20px] text-lg lg:text-xl lg:leading-[22px] h-[63px] lg:h-[50px] overflow-y-hidden mb-3 lg:mb-2">
+      <h2 className="leading-[20px] text-lg lg:text-xl lg:leading-[22px] h-[63px] lg:h-[48px] overflow-y-hidden mb-3 lg:mb-2">
         {data.title}
       </h2>
       <p className="leading-5 h-[63px] lg:h-[60px] overflow-y-hidden">
@@ -86,6 +86,7 @@ const MissedNews: React.FC<MissedNewsProps> = ({ newsData }) => {
       <div className="w-[80vw] md:flex md:space-x-4 space-y-4 md:space-y-0">
         <NewsItem data={newsData.sportsNews[8]} title={"sports"} id={8} />
         <NewsItem data={newsData.financeNews[8]} title={"finance"} id={8} />
+        <NewsItem data={newsData.financeNews[9]} title={"finance"} id={9} />
         <NewsItem
           data={newsData.technologyNews[8]}
           title={"technology"}
